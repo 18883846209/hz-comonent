cacdee52f26fe7434bc63e9d589566958b75fce5
zhangjie <1217336430@qq.com>
2020-04-08 15:02:56 +0800

    feat(warn):告警样式修复

--- example/simple/src/components/WarnItem/index.js
+++ example/simple/src/components/WarnItem/index.js
@@ -1,24 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Flex } from "antd-mobile";
 import { Progress, Item, Card } from "@/components/WarnComponents/index";
 import styles from "./styles/index.less";
 
 const Warn = ({ onClick }) => (
   <div className={styles.warn_item} onClick={onClick}>
     <div className={styles.top}>
-      <div className={styles.left}>姓名姓名姓名姓名姓名</div>
-      <div className={styles.right}>20秒</div>
+      <Flex justify="between">
+        <div className={styles.left}>姓名姓名姓名姓名姓名姓名姓名姓名姓名姓名</div>
+        <div className={styles.right}>20秒</div>
+      </Flex>
     </div>
-    <div className={styles.bottom}>
-      <Card style={{ marginRight: 10 }} text="抓拍照" />
-      <Card text="布控照" />
-      <div className={styles.right}>
-        <div style={{ height: 100 }}>
-          <Item desc="asdasdasd测试测试asdasdasd测试测试asdasdasd测试测试" />
-          <Item />
+    <div>
+      <Flex className={styles.bottom}>
+        <Card style={{ marginRight: 10 }} text="抓拍照" />
+        <Card text="布控照" />
+        <div className={styles.right}>
+          <div style={{ height: 100 }}>
+            <Item desc="asdasdasd测试测试asdasdasd测试测试asdasdasd测试测试" />
+            <Item />
+          </div>
+          <Progress />
         </div>
-        <Progress />
-      </div>
+      </Flex>
     </div>
   </div>
 );
