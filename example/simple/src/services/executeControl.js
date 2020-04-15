00c9e17f0185af1004530801d139d41f77ca4cad
fengyu <fengyu@123.com>
2020-04-15 10:19:01 +0800

    style(布控): 接口server build报错修改

--- example/simple/src/services/executeControl.js
+++ example/simple/src/services/executeControl.js
@@ -1,9 +1,10 @@
 import request from "../utils/request";
 
-const { server = "" } = window.hzConfig;
+// const { server = "" } = window.hzConfig;
+// const server = 'http://192.168.111.231:8080'
 
 /** 查询布控列表 */
-export async function getExecuteList(params = {}) {
+export async function getExecuteList(params = {}, server) {
   return request(`${server}/disposition/face/list`, {
     method: "POST",
     body: params
@@ -11,7 +12,7 @@ export async function getExecuteList(params = {}) {
 }
 
 /** 订阅/取消订阅 */
-export async function subscribe(params = {}) {
+export async function subscribe(params = {}, server) {
   return request(`${server}/disposition/subscribe`, {
     method: "POST",
     body: params
@@ -19,7 +20,7 @@ export async function subscribe(params = {}) {
 }
 
 /** 设备查询 */
-export async function getDevices(params = {}) {
+export async function getDevices(params = {}, server) {
   //192.168.111.231:8080
   return request(`${server}/disposition/devices`, {
     method: "POST",
