25d0407a5e1b3dab88ce21c76f917eda67b890fb
dengsha <dengsha_cqupt@163.com>
2020-04-15 17:58:43 +0800

    style(布控): 删除冗余代码

--- example/simple/src/pages/deviceList/index.js
+++ example/simple/src/pages/deviceList/index.js
@@ -9,23 +9,10 @@ const List = dynamic(import("@/components/List"), {
   ssr: false
 });
 
-// 虚拟数据
-const dataItem = {
-  device_name: "Meet hotel",
-  hierarchy_name: "不是所有的兼职汪都需要风吹日晒",
-  status: 2, //在线状态：0：登录中; 1：在线/启用; 2:离线/停用: 9:其他
-  device_id: "1"
-};
-const data = [];
-for (let i = 0; i < 15; i++) {
-  dataItem.key = i;
-  data.push(dataItem);
-}
-
 var params = {};
 
 const DeviceList = () => {
-  const [datas, setData] = useState(data);
+  const [datas, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [success, setSuccess] = useState(false);
 
@@ -41,21 +28,15 @@ const DeviceList = () => {
       body: params,
       method: "POST"
     };
-    if (params.device_ids) {
-      request(`${server}/disposition/devices`, object).then(res => {
-        setLoading(false);
-        if (!res.code || res.code.slice(-4) != "0000") {
-          setSuccess(false);
-        } else {
-          setSuccess(true);
-          setData(res.data);
-        }
-      });
-    } else {
+    request(`${server}/disposition/devices`, object).then(res => {
       setLoading(false);
-      setSuccess(true);
-      setData([]);
-    }
+      if (!res.code || res.code.slice(-4) != "0000") {
+        setSuccess(false);
+      } else {
+        setSuccess(true);
+        setData(res.data);
+      }
+    }); 
   }
 
   //获取item进行展示
@@ -74,7 +55,6 @@ const DeviceList = () => {
           </div>
           <div className={styles.titleDiv}>{item.device_name}</div>
         </div>
-
         <div className={styles.contentDiv}>{item.hierarchy_name}</div>
       </div>
     );
@@ -107,30 +87,3 @@ const DeviceList = () => {
 };
 
 export default DeviceList;
-
-{
-  /* <ListView
-          ref={dateRef}
-          dataSource={dataSource}
-          // renderHeader={() => <span>header</span>}
-          renderFooter={() => (
-            <div style={{ padding: 30, textAlign: "center" }}>{upLoading ? "Loading..." : "Loaded"}</div>
-          )}
-          // renderSectionHeader={sectionData => <div>{`Task ${sectionData.split(" ")[1]}`}</div>}
-          // renderBodyComponent={() => <MyBody />}
-          renderRow={renderRow}
-          // renderSeparator={separator}
-          style={{
-            height: 500,
-            overflow: "auto"
-          }}
-          pageSize={4}
-          onScroll={(value) => {
-            console.log("scroll", value);
-          }}
-          scrollRenderAheadDistance={500}
-          onEndReached={onEndReached}
-          onEndReachedThreshold={10}
-          useBodyScroll={true}
-        /> */
-}
