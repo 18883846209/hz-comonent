56f16f11ab6221635c1a176618130b3e310c49de
fengyu <fengyu@123.com>
2020-04-15 09:38:38 +0800

    style(布控): 接口server

--- example/simple/src/services/executeControl.js
+++ example/simple/src/services/executeControl.js
@@ -1,8 +1,10 @@
 import request from "../utils/request";
 
+const { server = "" } = window.hzConfig;
+
 /** 查询布控列表 */
 export async function getExecuteList(params = {}) {
-  return request(`http://192.168.100.205:8080/disposition/face/list`, {
+  return request(`${server}/disposition/face/list`, {
     method: "POST",
     body: params
   });
@@ -10,7 +12,7 @@ export async function getExecuteList(params = {}) {
 
 /** 订阅/取消订阅 */
 export async function subscribe(params = {}) {
-  return request(`http://192.168.100.205:8080/disposition/subscribe`, {
+  return request(`${server}/disposition/subscribe`, {
     method: "POST",
     body: params
   });
@@ -19,7 +21,7 @@ export async function subscribe(params = {}) {
 /** 设备查询 */
 export async function getDevices(params = {}) {
   //192.168.111.231:8080
-  return request(`http://192.168.111.231:8080/disposition/devices`, {
+  return request(`${server}/disposition/devices`, {
     method: "POST",
     body: params
   });
