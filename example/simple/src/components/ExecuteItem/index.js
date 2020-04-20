89a40c53bc41b9f53090c0dba39261682c3e662f
fengyu <fengyu@123.com>
2020-04-20 10:36:01 +0800

    feat(app):updatestyle

--- example/simple/src/components/ExecuteItem/index.js
+++ example/simple/src/components/ExecuteItem/index.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import List from "@/components/List";
 import { Flex } from "antd-mobile";
 import PropTypes from "prop-types";
 import classnames from "classnames";
-import { getRedirectStatus } from "@/utils/common";
+import TouchFeedback from "rmc-feedback";
 import moment from "moment";
 import router from "next/router";
+import { getRedirectStatus } from "@/utils/common";
+import List from "@/components/List";
 import { getCalculateTime } from "@/utils/utils";
 import styles from "./styles/index.less";
 
@@ -35,53 +36,63 @@ const ListItem = ({ item, imgUrl, changeImgUrl, currentId, subscribeHandler }) =
   };
 
   return (
-    <div className={styles.item} onClick={() => toDetails(item)} key={item.disposition_id}>
-      <Flex className={styles["list-item"]} key={item.disposition_id}>
-        <div
-          className={
-            item.disposition_target_type === 1
-              ? classnames(styles["item-name-list"], styles["item-left"])
-              : styles["item-left"]
-          }
-        >
-          {item.disposition_target_type === 1 ? (
-            <div className={styles["name-list"]}>
-              <span className={styles["name-list-name"]}>{nameListToStr(item.tabs)}</span>
-            </div>
-          ) : (
-            <img className={styles.img} src={item.image} />
-          )}
-        </div>
-        <div className={styles["item-right"]}>
-          <div>
-            <span className={styles.name}>{item.title}</span>
-            <span className={styles.state}>{getRedirectStatus(item.disposition_status)}</span>
+    <TouchFeedback activeClassName="active">
+      <div className={styles.item} onClick={() => toDetails(item)} key={item.disposition_id}>
+        <Flex className={styles["list-item"]} key={item.disposition_id}>
+          <div
+            className={
+              item.disposition_target_type === 1
+                ? classnames(styles["item-name-list"], styles["item-left"])
+                : styles["item-left"]
+            }
+          >
+            {item.disposition_target_type === 1 ? (
+              <div className={styles["name-list"]}>
+                <span className={styles["name-list-name"]}>{nameListToStr(item.tabs)}</span>
+              </div>
+            ) : (
+              <img className={styles.img} src={item.image} />
+            )}
           </div>
-          <div className={styles.card}>
-            <div className={styles.date}>{getTime(item.start_time, item.end_time)}</div>
-            <div className={styles.detail}>
-              <img style={{ height: 14, width: 14, marginRight: "2%", verticalAlign: "bottom" }} src="/static/2x/user.png" />
-              <span>{item.owner}</span>
-              <img style={{ height: 14, width: 14, margin: "0 2%", verticalAlign: "bottom" }} src="/static/2x/time.png" />
-              <span>{getCalculateTime(item.create_time)}</span>
-              <span
-                className={styles.operation}
-                onClick={e => subscribeHandler(item.disposition_id, item.subscribe_status ? 0 : 1, e)}
-              >
+          <div className={styles["item-right"]}>
+            <div>
+              <span className={styles.name}>{item.title}</span>
+              <span className={styles.state}>{getRedirectStatus(item.disposition_status)}</span>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.date}>{getTime(item.start_time, item.end_time)}</div>
+              <div className={styles.detail}>
                 <img
-                  src={
-                    currentId === item.disposition_id
-                      ? `/static/2x/${imgUrl}`
-                      : `/static/2x/${changeImgUrl(Number(item.subscribe_status))}`
-                  }
-                  style={{ height: 18, width: 16 }}
+                  style={{ height: 14, width: 14, marginRight: "2%", verticalAlign: "bottom" }}
+                  src="/static/2x/user.png"
                 />
-              </span>
+                <span>{item.owner}</span>
+                <img
+                  style={{ height: 14, width: 14, margin: "0 2%", verticalAlign: "bottom" }}
+                  src="/static/2x/time.png"
+                />
+                <span>{getCalculateTime(item.create_time)}</span>
+                <TouchFeedback activeClassName="active">
+                  <span
+                    className={styles.operation}
+                    onClick={e => subscribeHandler(item.disposition_id, item.subscribe_status ? 0 : 1, e)}
+                  >
+                    <img
+                      src={
+                        currentId === item.disposition_id
+                          ? `/static/2x/${imgUrl}`
+                          : `/static/2x/${changeImgUrl(Number(item.subscribe_status))}`
+                      }
+                      style={{ height: 18, width: 16 }}
+                    />
+                  </span>
+                </TouchFeedback>
+              </div>
             </div>
           </div>
-        </div>
-      </Flex>
-    </div>
+        </Flex>
+      </div>
+    </TouchFeedback>
   );
 };
 
--- example/simple/src/components/PullLoading/index.js
+++ example/simple/src/components/PullLoading/index.js
@@ -1,13 +1,7 @@
 import { useState, useEffect } from "react";
 
-function Loading({ refreshing }) {
-  const [src, setSrc] = useState("");
-  const [refresh, setRefresh] = useState(refreshing);
-  useEffect(() => {
-    setSrc("/static/2x/loading.gif");
-    setRefresh(refreshing);
-  }, [refreshing]);
-  return refresh ? <img style={{ height: 25 }} src={src} /> : null;
+function Loading() {
+  return <img style={{ height: 25 }} src={`/static/2x/loading.gif?${new Date().valueOf()}`} />;
 }
 
 export default Loading;
